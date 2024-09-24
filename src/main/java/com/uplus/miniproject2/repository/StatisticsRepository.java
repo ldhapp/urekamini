e6d9c1212e4305bc9ea2147d5f6b783e424f578b
leedohyun <lucky77795@naver.com>
2024-09-24 16:18:40 +0900

    fix: 프로필 요청이 Approved가 아닐 경우 정보통계에 반영되지 않도록 수정.

--- src/main/java/com/uplus/miniproject2/repository/StatisticsRepository.java
+++ src/main/java/com/uplus/miniproject2/repository/StatisticsRepository.java
@@ -9,20 +9,40 @@ import java.util.List;
 
 @Repository
 public interface StatisticsRepository extends JpaRepository<Profile, Long> {
+
+    @Query("SELECT p FROM Profile p JOIN ProfileRequest pr ON pr.profile = p "
+            + "WHERE pr.requestStatus NOT IN ('PENDING', 'REJECTED')")
+    List<Profile> findAll();
+
     // 전공별 수를 가져오는 쿼리
-    @Query("SELECT p.major AS major, COUNT(p) AS count FROM Profile p GROUP BY p.major")
+    @Query("SELECT p.major AS major, COUNT(p) AS count " +
+            "FROM Profile p " +
+            "JOIN ProfileRequest pr ON pr.profile = p " +
+            "WHERE pr.requestStatus NOT IN ('PENDING', 'REJECTED') " +
+            "GROUP BY p.major")
     List<Object[]> findMajorCounts();
 
     // 전공 목록 출력 (중복 제거)
-    @Query("SELECT DISTINCT p.major FROM Profile p")
+    @Query("SELECT DISTINCT p.major FROM Profile p " +
+            "JOIN ProfileRequest pr ON pr.profile = p " +
+            "WHERE pr.requestStatus NOT IN ('PENDING', 'REJECTED')")
     List<String> findDistinctMajors();
 
     // 취미
-    @Query("SELECT h.name, COUNT(p) FROM Profile p JOIN p.hobbies h GROUP BY h.name")
+    @Query("SELECT h.name, COUNT(p) AS count " +
+            "FROM Profile p " +
+            "JOIN ProfileRequest pr ON pr.profile = p " +
+            "JOIN p.hobbies h " +
+            "WHERE pr.requestStatus NOT IN ('PENDING', 'REJECTED') " +
+            "GROUP BY h.name")
     List<Object[]> findHobbyCounts();
 
     // 거주지역
-    @Query("SELECT p.region.name, COUNT(p) FROM Profile p GROUP BY p.region.name")
+    @Query("SELECT p.region.name, COUNT(p) AS count " +
+            "FROM Profile p " +
+            "JOIN ProfileRequest pr ON pr.profile = p " +
+            "WHERE pr.requestStatus NOT IN ('PENDING', 'REJECTED') " +
+            "GROUP BY p.region.name")
     List<Object[]> findRegionCounts();
 
 }
