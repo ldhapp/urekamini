2b7d398a00d0107cd91823e95e6737df1397e7ea
leedohyun <lucky77795@naver.com>
2024-09-24 16:08:25 +0900

    fix: 프로필 요청이 Approved가 아닐 경우 Map에 보이지 않도록.

--- src/main/java/com/uplus/miniproject2/repository/MapRepository.java
+++ src/main/java/com/uplus/miniproject2/repository/MapRepository.java
@@ -14,6 +14,10 @@ public interface MapRepository extends JpaRepository<Profile, Long> {
     // 특정 사용자 ID로 프로필 조회
     Optional<List<Profile>> findByUserId(Long userId);
 
-    @Query("SELECT p FROM Profile p JOIN FETCH p.user JOIN FETCH p.region")
+    @Query("SELECT p FROM Profile p "
+            + "JOIN FETCH p.user "
+            + "JOIN FETCH p.region "
+            + "JOIN ProfileRequest pr ON pr.profile = p "
+            + "WHERE pr.requestStatus NOT IN ('PENDING', 'REJECTED')")
     List<Profile> findAll();
 }
