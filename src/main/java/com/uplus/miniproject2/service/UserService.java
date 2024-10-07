b15a6b8c17726016b1c9d136110d5a0512af4dde
leedohyun <lucky77795@naver.com>
2024-10-07 10:31:50 +0900

    docs: 주석 작성 연습

--- src/main/java/com/uplus/miniproject2/service/UserService.java
+++ src/main/java/com/uplus/miniproject2/service/UserService.java
@@ -15,6 +15,27 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * @Project : 유레카 미니 프로젝트 2
+ * @package_name        : com.uplus.miniproject2.service
+ * @FileName : UserService
+ * @since : 2024. 09. 27.
+ * @version 1.0
+ * @author : 이도현
+ * @프로그램 설명 : 유저 아이디 값을 통해 해당 아이디 값 유저의 프로필을 조회하거나, 검색 조건에 따른 유저 정보 및 프로필을 조회할 수 있다.
+ * @see com.uplus.miniproject2.mapper.UserMapper
+ * @see com.uplus.miniproject2.repository.CustomUserRepository
+ *
+ * <pre>
+ * << 개정이력(Modification Information) >>
+ *
+ *  수정일        수정자      수정내용
+ *  ----------   --------   ---------------------------
+ *  2024.09.27   이도현      최초생성
+ *  2024.09.27   이도현      검색 조건 별 조회 구현
+ *  2024.10.01   이도현      QueryDSL -> MyBatis로 수정
+ *  </pre>
+ */
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -23,6 +44,19 @@ public class UserService {
     private final CustomUserRepository userRepository;
     private final UserMapper userMapper;
 
+    /**
+     * @Method Name : searchUser
+     * @date : 2024. 09. 27.
+     * @Method 설명 :
+     * <p>
+     * 이름, MBTI, 전공, 성별 값을 검색어로 해당하는 유저를 Repository를 통해 조회한다.
+     * </p>
+     * @param name: 검색으로 사용할 수 있는 이름 (이름에 해당 글자가 포함되면 검색)
+     * @param mbti: 검색으로 사용할 수 있는 MBTI (16개의 MBTI중 1개 선택)
+     * @param major: 검색으로 사용할 수 있는 전공 (전공에 해당 글자가 포함되면 검색)
+     * @param gender: 검색으로 사용할 수 있는 성별값 (남, 여중 택 1)
+     * @return Page<MainPageUserDto>: 페이지네이션을 위해 조회 결과 DTO 값을 Page객체에 담아 반환한다.
+     */
     public Page<MainPageUserDto> searchUser(String name, String mbti, String major, String gender, Pageable pageable) {
         return userRepository.findBySearchCondition(name, mbti, major, gender, pageable);
     }
